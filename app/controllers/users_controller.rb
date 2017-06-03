499a9ade1f8f250ca00bdd9a0baa0fcf341792f7
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-03 14:06:28 +0900

    add authorization for user edit and update actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
+  # 本人以外はeditアクションとupdateアクションを実行できないようにする
+  before_action :correct_user, only: [:edit, :update]
+
   def edit
   end
 
   def update
+  end
+
+  private
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
   end
 end
