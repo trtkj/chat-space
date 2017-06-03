8f7a507ac3531ab06e3bac1c606a95481ceda06e
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-03 14:56:52 +0900

    delete redundant code

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,9 +6,8 @@ class UsersController < ApplicationController
   end
 
   def update
-    user = User.find(params[:id])
-    user.update(user_params)
-    redirect_to controller: :messages, action: :index
+    @user.update(user_params)
+    redirect_to(root_url)
   end
 
   private
