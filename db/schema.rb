cf7eb521f719ca1be2bf55a7e2a579038656c901
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-05 15:48:05 +0900

    configure NOT NULL to name column

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 20170605062015) do
 
   create_table "groups", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.string   "name"
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
