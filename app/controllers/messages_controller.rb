bbd590117423b682541db33f623b0fa6b756d71b
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-06 20:59:40 +0900

    define messages index and create actions

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,8 +1,16 @@
 class MessagesController < ApplicationController
   def index
     @message = Message.new
+    # groupのidは仮置き
+    @group = Group.find(1)
   end
 
   def create
+    @message = Message.create(message_params)
+  end
+
+  private
+  def message_params
+    params.require(:message).permit(:body, :image).merge(group_id: params[:group_id], user_id: current_user.id)
   end
 end
