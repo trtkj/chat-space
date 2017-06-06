ad23cd64d128a30a48eb40459672369ee23eb570
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-06 17:13:31 +0900

    configure routes for messages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
+  end
   root    "messages#index"
 end
