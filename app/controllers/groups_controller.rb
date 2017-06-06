cc27e6fd70b5c114f5ce64cf79909e6f75e1f6e0
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-06 14:51:50 +0900

    define group edit/update actions

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -4,7 +4,7 @@ class GroupsController < ApplicationController
   end
 
   def create
-    @group = Group.new(create_params)
+    @group = Group.new(group_params)
     if @group.save
       redirect_to :root, notice: "グループを作成しました"
     else
@@ -13,13 +13,20 @@ class GroupsController < ApplicationController
   end
 
   def edit
+    @group = Group.find(params[:id])
   end
 
   def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to :root, notice: "グループを編集しました"
+    else
+      render :edit
+    end
   end
   
   private
-  def create_params
+  def group_params
     params.require(:group).permit(:name, { user_ids: [] })
   end
 end
