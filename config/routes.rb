4bd17e14535f7fa33e3a4159d0f7e858eb4116a4
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-07 17:31:52 +0900

    Replace root configuration on top

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
+  root    "groups#index"
   devise_for :users
   resources :users, only: [:edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
     resources :messages, only: [:index, :create]
   end
-  root    "groups#index"
 end
