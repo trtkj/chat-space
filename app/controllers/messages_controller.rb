0fd110a1141aced3d17b16635f74a9aa061fa85e
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-07 17:55:06 +0900

    Create group instance on fail in message sending

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,7 @@
 class MessagesController < ApplicationController
   def index
-    @message = Message.new
     @group = Group.find(params[:group_id])
+    @message = Message.new
   end
 
   def create
@@ -9,6 +9,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to group_messages_path, notice: "メッセージを投稿しました"
     else
+      @group = Group.find(params[:group_id])
       flash.now[:alert] = "メッセージを入力してください"
       render :index
     end
