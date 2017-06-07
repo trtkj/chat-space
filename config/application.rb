4d2932f5c47cc03217eee6f47dfbf959d4ceaed7
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-07 16:36:33 +0900

    Configure not to generate unnecessary div when validation error

--- config/application.rb
+++ config/application.rb
@@ -12,5 +12,6 @@ module ChatSpace
       g.test_framework false
     end
     config.i18n.default_locale = :ja
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| %Q(#{html_tag}).html_safe }
   end
 end
