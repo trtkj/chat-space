3cf83cef52743c64b5bb3678c81665f12fe2df17
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-08 12:40:35 +0900

    Install bullet gem to detect unnecessary queries

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,10 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  #bullet用に追加
+  config.after_initialize do
+    Bullet.enable  = true
+    Bullet.console = true
+  end
 end
