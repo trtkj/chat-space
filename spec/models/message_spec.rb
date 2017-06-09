95dab7e72c36e6dce867e9c7b705b5b4b742b724
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-09 19:41:23 +0900

    Define a variable

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -2,30 +2,33 @@ require "rails_helper"
 
 describe Message do
   describe "#create" do
+    let(:message) do
+        create(:message)
+    end
 
     # メッセージを保存できる場合
     ## メッセージがあれば画像がなくても保存できる
     it "is valid with body" do
-        message = create(:message, image: nil)
+        message.image = nil
         expect(message).to be_valid
     end
 
     ## 画像があればメッセージがなくても保存できる
     it "is valid with image" do
-        message = create(:message, body: nil)
+        message.body = nil
         expect(message).to be_valid
     end
 
     ## メッセージと画像があれば保存できる
     it "is valid with body and image" do
-        message = create(:message)
         expect(message).to be_valid
     end
 
     # メッセージを保存できない場合
     ## メッセージも画像も無いと保存できない
     it "is invalid without both body and image" do
-        message = build(:message, body: nil, image: nil)
+        message.body = nil
+        message.image = nil
         message.valid?
         expect(message.errors[:body][0]).to include("を入力してください")
     end
