12c08ea4dcfc353b71824b47ec62ea9483be3059
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-09 17:30:30 +0900

    Modify image assignment for FactoryGirl

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
 
   factory :message do
     body      "イルカかわいい"
-    image     "iruka.jpg"
+    image     { Rack::Test::UploadedFile.new("#{Rails.root}/spec/fixtures/img/iruka.jpg", 'image/jpg') }
     group_id  "1"
     user_id   "1"
   end
