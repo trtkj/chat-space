96dc7f5ec8d36206f2b062b9c4e02a1ae963b9c9
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-10 16:27:34 +0900

    Modify messages factory

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -3,8 +3,8 @@ FactoryGirl.define do
   factory :message do
     body      "イルカかわいい"
     image     { Rack::Test::UploadedFile.new("#{Rails.root}/spec/fixtures/img/iruka.jpg", 'image/jpg') }
-    group_id  "1"
-    user_id   "1"
+    group
+    user
   end
 
 end
