91e1b4e903f7e6667fec2e51a3495f228efcd493
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-10 16:29:21 +0900

    Add variables and login process and Comment out

--- spec/controllers/messages_controller_spec.rb
+++ spec/controllers/messages_controller_spec.rb
@@ -1,78 +1,83 @@
 require 'rails_helper'
 
 
-describe MessageController, type: :controller do
-
+describe MessagesController, type: :controller do
   # ログインユーザー
   describe "sign_in user access" do
+    let(:message){create(:message)}
+    let(:user){User.find(message.user_id)}
+    let(:group){Group.find(message.group_id)}
+
+    before do
+      login_user user
+    end
 
     describe "GET #index" do
+      # # @groupに要求されたグループが割り当てられること
+      # it "assigns the requested group to @group" do
+      # end
 
-      # @groupに要求されたグループが割り当てられること
-      it "assigns the requested group to @group" do
-      end
+      # # @messageに新しいメッセージが割り当てられること
+      # it "assigns a new message to @message" do
+      # end
 
-      # @messageに新しいメッセージが割り当てられること
-      it "assigns a new message to @message" do
-      end
-
-      # :indexテンプレートを表示すること
-      it "renders the :index template"
-      end
+      # # :indexテンプレートを表示すること
+      # it "renders the :index template" do
+      # end
     end
 
-    describe "POST #create" do
+    # describe "POST #create" do
 
-      # @groupに要求されたグループが割り当てられること
-      it "assigns the requested group to @group" do
-      end
+    #   # @groupに要求されたグループが割り当てられること
+    #   it "assigns the requested group to @group" do
+    #   end
 
-      # @messageに要求されたメッセージが割り当てられること
-      it "assigns the requested message to @message" do
-      end
+    #   # @messageに要求されたメッセージが割り当てられること
+    #   it "assigns the requested message to @message" do
+    #   end
 
-      # 送信成功時
-      context "with valid attributes" do
+    #   # 送信成功時
+    #   context "with valid attributes" do
 
-        # 新しいメッセージをDBに保存すること
-        it "saves the new message in the database" do
-        end
+    #     # 新しいメッセージをDBに保存すること
+    #     it "saves the new message in the database" do
+    #     end
 
-        # :indexテンプレートを表示すること
-        it "renders the :index template" do
-        end
-      end
+    #     # :indexテンプレートを表示すること
+    #     it "renders the :index template" do
+    #     end
+    #   end
 
-      # 送信失敗時
-      context "with invalid attributes" do
+    #   # 送信失敗時
+    #   context "with invalid attributes" do
 
-        # メッセージをDBに保存しないこと
-        it "doesn't save the message" do
-        end
+    #     # メッセージをDBに保存しないこと
+    #     it "doesn't save the message" do
+    #     end
 
-        # :index テンプレートを表示すること
-        it "renders the :index template" do
-        end
-      end
-    end
+    #     # :index テンプレートを表示すること
+    #     it "renders the :index template" do
+    #     end
+    #   end
+    # end
   end
 
-  # ゲストユーザー
-  describe "guest access" do
+  # # ゲストユーザー
+  # describe "guest access" do
 
-    describe "GET #index" do
+  #   describe "GET #index" do
 
-      # サインイン画面にリダイレクトさせること
-      it "redirects to sign_in template" do
-      end
-    end
+  #     # サインイン画面にリダイレクトさせること
+  #     it "redirects to sign_in template" do
+  #     end
+  #   end
 
-    describe "POST #create" do
-      
-      # サインイン画面にリダイレクトさせること
-      it "redirects to sign_in template" do
-      end
-    end
-  end
+  #   describe "POST #create" do
+
+  #     # サインイン画面にリダイレクトさせること
+  #     it "redirects to sign_in template" do
+  #     end
+  #   end
+  # end
 end
 
