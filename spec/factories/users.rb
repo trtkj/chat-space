4244dc1b12fde38e62d78b34aaea39f1912558b5
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-10 16:27:19 +0900

    Define user factory

---
+++ spec/factories/users.rb
@@ -0,0 +1,10 @@
+FactoryGirl.define do
+
+  factory :user do
+    name      { Faker::Name.name }
+    email     { Faker::Internet.email }
+    password  { Faker::Internet.password(8) }
+    password_confirmation { Faker::Internet.password(8) }
+  end
+
+end
