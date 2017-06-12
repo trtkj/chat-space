e068991ab079a1ebca7d6b80ecba4396a6f8c0c5
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-12 17:41:36 +0900

    Define ajax process on success

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,13 @@
 $(function(){
+  function buidlHTML(data){
+    var span_name = $('<span class="message__name">').append(data.user_name);
+    var span_date = $('<span class="message__date">').append(data.updated_at);
+    var p_text = $('<p class="message__text">').append(data.body);
+    var img = $('<img>').attr('src', data.image);
+    var html = $('<li class="message">').append(span_name).append(span_date).append(p_text).append(img);
+    return html;
+  }
+
   $(".post").on("submit", function(e){
     e.preventDefault();
     var url = $(".post").attr("action");
@@ -10,6 +19,12 @@ $(function(){
       processData: false,
       contentType: false,
       dataType: "json"
+    }).done(function(data){
+      console.log(data);
+      var html = buidlHTML(data);
+      $(".messages").append(html);
+    })
+    .fail(function(){
     });
   });
 });
