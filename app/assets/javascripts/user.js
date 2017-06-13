873f70d8b635c0bc4ffe4a8eb9e50d36e66e63c3
trtkj <tsurutakoji@tsurutakoujinoMacBook-Pro.local>
2017-06-13 12:41:43 +0900

    Modify form

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -1,6 +1,7 @@
 $(function(){
   $(".chat-group-form__input").on("keyup", function() {
-    var input = $("#group_members").val();
+    var input = $("#user_search").val();
+    console.log(input);
     var url = "/users"
     $.ajax({
       type: "GET",
